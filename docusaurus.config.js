fcedcd0a3a5edcc829adf6c69d4eebf6ffe1b84e
Betuel <116360280+betuel-terainsights@users.noreply.github.com>
2025-11-04 14:58:57 +0200

    page link base URL organization and project update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://betuelgreen.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/web/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Betuel Green', // Usually your GitHub org/user name.
+  projectName: 'Portfolio Website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
